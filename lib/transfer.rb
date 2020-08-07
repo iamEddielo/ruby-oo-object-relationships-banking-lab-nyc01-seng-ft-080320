87acf75f1721298ce4d2367a3ca66c6c25a03cd7
slurio <samantha.lurio@gmail.com>
2020-08-07 13:46:23 -0400

    m

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,13 +7,35 @@ class Transfer
     @sender= sender
     @receiver= receiver
     @status= "pending"
-    @amount = amount
+    @amount= amount
   end
 
-  
+  def valid?
+    self.sender.valid? && self.receiver.valid? 
+  end
 
+  def execute_transaction
+    if  valid? && self.status == "pending" && self.sender.balance > self.amount
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
     
-    
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
 
 
 end
+
+#Sam= BankAccount.new("sam") #sender
+#Eddie = BankAccount.new("eddie") #receiver
+#transfer_one = Transfer(Sam, Eddie, 100)
